7fb832424f3eee7e5a7c330848b5b590dc3f301c
Juan Tubert <jtubert@tombras.com>
2025-06-11 14:54:43 -0400

    deploying on 2025-06-11 14:54:43

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,1 +1,1 @@
-~/Desktop/ffmpeg -i assets/google.mov -vcodec libx264 -preset fast -crf 23 -acodec aac -b:a 192k google3.mp4
+~/Desktop/ffmpeg -i assets/meataversity.mov -vcodec libx264 -preset fast -crf 23 -acodec aac -b:a 192k meataversity.mp4
